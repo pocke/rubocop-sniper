ff1719ed6d202b5cdf9376dacf7eb2acf468c35a
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-12-11 22:22:52 +0900

    Init

---
+++ lib/rubocop/sniper.rb
@@ -0,0 +1,7 @@
+require "rubocop/sniper/version"
+
+module Rubocop
+  module Sniper
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/sniper/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Sniper
+    VERSION = "0.1.0"
+  end
+end
